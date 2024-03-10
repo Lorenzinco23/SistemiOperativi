c2aee935b9b898de4d8acf96abe4f016e2d52137
Lorenzinco23 <lorenzinco23@gmail.com>
2024-03-11 00:07:23 +0100

    found bug, to fix

--- test.c
+++ test.c
@@ -1,5 +1,8 @@
 #include "headers/my_malloc.h"
 #include <stdio.h>
+#include <assert.h>
+
+#define MEGABYTE 1024*1024
 
 typedef struct{
   int a;
@@ -9,6 +12,7 @@ typedef struct{
 
 int main() {
   
+  //buddy allocator tests
   int *ptr = (int *)my_malloc(sizeof(int));
   *ptr = 42;
   printf("%d\n", *ptr);
@@ -20,4 +24,15 @@ int main() {
 
   my_free(ptr);
   my_free(my_struct);
+
+  //mmap allocator tests
+  void *ptr2 = my_malloc(MEGABYTE * 2);
+  printf("%p\n", ptr2);
+  my_free(ptr2);
+
+  //max size allocation after free test
+  //this test is to check if the allocator can allocate the maximum size after a free
+  void *ptr3 = my_malloc(MEGABYTE-1);
+  printf("%p\n", ptr3);
+  assert(ptr3 == ptr);
 }
